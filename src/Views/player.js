ee7ad9639cc077a3c733bb774bf911a08ef00b8c
samir benzada <samirbenzada@gmail.com>
2019-10-17 14:41:41 -0400

    update

--- src/Views/player.js
+++ src/Views/player.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
 import music from '../images/music.jpg';
+import vinyl from '../images/vinyl.png';
+import runMario from '../images/mario.gif';
 
 const Player = () => {
 
@@ -14,6 +16,8 @@ const Player = () => {
     const [pauseBtn, setPauseBtn] = useState("d-none");
     const [loopBtn, setLoopBtn] = useState('col looop');
     const [stopLoopBtn, setStopLoopBtn] = useState('d-none');
+    const [album, setAlbum] = useState("albumSpin");
+    const [marioGonnaRun, setMarioGonnaRun] = useState("mario");
 
     async function fetchData() {
         const res = await fetch("https://assets.breatheco.de/apis/sound/songs");
@@ -32,16 +36,23 @@ const Player = () => {
         let ind = Number(e.target.dataset.index);
         let name = e.target.dataset.name;
         let url = "https://assets.breatheco.de/apis/sound/"+mus;
+        // let firstMusicToPlay = new Audio(url);
+        // firstMusicToPlay.play();
         setMymusic(new Audio(url));
         setMyMusicName(name);
         setIndexMyMusic(ind);
     }
+
+    console.log(myMusic);
+    
 
     const playMusicHandler = () => {
         if (myMusic) {
             myMusic.play();
             setPlayBtn("d-none");
             setPauseBtn("col btnPlay");
+            setAlbum("albumSpin rotate");
+            setMarioGonnaRun("mario run");
         } else {
             setMsg("Choose a music you IDIOT!");
             setTimeout(() => {
@@ -54,6 +65,8 @@ const Player = () => {
         myMusic.currentTime = 0;
         setPlayBtn("col btnPlay");
         setPauseBtn("d-none");
+        setAlbum("albumSpin");
+        setMarioGonnaRun("d-none");
     }
     const loopAudio = () => {
         if (myMusic) {
@@ -86,6 +99,7 @@ const Player = () => {
         myMusic.currentTime = 0;
         setPlayBtn("col btnPlay");
         setPauseBtn("d-none");
+        setAlbum("albumSpin");
         setMyMusicName(nexName);
         setMymusic('');
         setMymusic(new Audio(newSong)); 
@@ -109,6 +123,7 @@ const Player = () => {
         myMusic.currentTime = 0;
         setPlayBtn("col btnPlay");
         setPauseBtn("d-none");
+        setAlbum("albumSpin");
         setMyMusicName(nexName);
         setMymusic('');
         setMymusic(new Audio(newSong)); 
@@ -119,56 +134,68 @@ const Player = () => {
         <>
         {dataError}
         <div className="container mt-5">
-            <div className="card player">
-                <img src={music} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <div className="alert alert-primary text-center" role="alert">
-                        {myMusicName}
-                        <span className="badge badge-pill badge-danger">{msg}</span>
-                    </div>
-                    <div className="row text-center">
-                        <div onClick={prevMusic} className="col">
-                            <i className="fas fa-step-backward fa-2x"></i>
-                        </div>
-                        <div onClick={playMusicHandler} className={playBtn}>
-                            <i className="fas fa-play fa-2x"></i>
-                        </div>
-                        <div onClick={pauseMusicHandler} className={pauseBtn}>
-                            <i className="fas fa-pause fa-2x"></i>
-                        </div>
-                        <div onClick={nextMusic} className="col">
-                            <i className="fas fa-step-forward fa-2x"></i>
-                        </div>
-                        <div onClick={loopAudio} className={loopBtn}>
-                            <i className="fas fa-sync"></i>
-                        </div>
-                        <div onClick={stopLoopAudio} className={stopLoopBtn}>
-                            <i className="fas fa-ban"></i>
+            <div className="row">
+
+                <div className="col-4">
+                    <img className={album} src={vinyl} alt="..."/>
+                </div>
+
+                <div className="col-4">
+                    <div className="card player">
+                        <img src={music} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <div className="alert alert-primary text-center" role="alert">
+                                {myMusicName}
+                                <span className="badge badge-pill badge-danger">{msg}</span>
+                            </div>
+                            <div className="row text-center">
+                                <div onClick={prevMusic} className="col prev">
+                                    <i className="fas fa-step-backward fa-2x"></i>
+                                </div>
+                                <div onClick={playMusicHandler} className={playBtn}>
+                                    <i className="fas fa-play fa-2x"></i>
+                                </div>
+                                <div onClick={pauseMusicHandler} className={pauseBtn}>
+                                    <i className="fas fa-pause fa-2x"></i>
+                                </div>
+                                <div onClick={nextMusic} className="col next">
+                                    <i className="fas fa-step-forward fa-2x"></i>
+                                </div>
+                                <div onClick={loopAudio} className={loopBtn}>
+                                    <i className="fas fa-sync"></i>
+                                </div>
+                                <div onClick={stopLoopAudio} className={stopLoopBtn}>
+                                    <i className="fas fa-ban"></i>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            
 
-            <ul className="list-group listSongs">{!songs ? ("loading...") : songs.map((item, index) => {
-                return (
-                    <li 
-                        key={index} 
-                        className="list-group-item list-group-item-action" 
-                        onClick={switchMusic} 
-                        data-url={item.url} 
-                        data-name={item.name}
-                        data-index={index}
-                    >
-                        {index+1} - {item.name}
-                    </li>
-                )
-            })}</ul>
+                <div className="col-4">
+                    <ul className="list-group listSongs">{!songs ? ("loading...") : songs.map((item, index) => {
+                        return (
+                                <li 
+                                    key={index} 
+                                    className="list-group-item list-group-item-action" 
+                                    onClick={switchMusic} 
+                                    data-url={item.url} 
+                                    data-name={item.name}
+                                    data-index={index}
+                                >
+                                    {index+1} - {item.name}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
 
+            </div> 
         </div>
 
-        
-        
+        <div className={marioGonnaRun}>
+            <img src={runMario} width="100px" height="100px" alt="witch" />
+        </div>
         </>
     );
 };
